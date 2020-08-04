6be0ed8802c54c86f13ded847ee8a0f3f6d403f4
bportilla <b.a.portilla.revelo@rug.nl>
2020-08-04 20:03:47 +0200

    radsyma

--- radsyma.py
+++ radsyma.py
@@ -12,6 +12,16 @@ plt.style.use('fancy')
 
 
 def toAU(x,y,xc,yc,pxsize,d):
+
+    ############################################################
+    #
+    # d: distance to the source in pc
+    # pxsize: pixel scale in arcsec/px
+    #
+    # Return: Length in AU
+    #
+    ############################################################
+
     dr=((x-xc)**2+(y-yc)**2)**0.5
     dr=(dr*pxsize)
     dr=(dr*units.arcsec).to(units.rad).value
@@ -23,15 +33,28 @@ def toAU(x,y,xc,yc,pxsize,d):
 
 
 def topx(l,pxsize,d):
+
+    ############################################################
+    #
+    # d: distance to the source in pc
+    # pxsize: pixel scale in arcsec/px
+    #
+    # Return: Length in pixels
+    #
+    ############################################################
+
     x=(l*units.au).to(units.pc).value
     x=((x/d)*units.rad).to(units.arcsec).value
     x=x/pxsize
     return x
 
 
-def get_profile(file,pxsize,PA_aperture,PA_disk,inc,d,size,Nbins,**kwargs):
+def get_profile(file,pxsize,PA_disk,inc,d,size,Nbins,dr,**kwargs):
 
     ############################################################
+    # 
+    # Extract a radial cut of the brightness along diffent 
+    # position angles.
     # 
     # file: the fits file of the observation
     # pxsize: pixel scale (arcsec/px)
@@ -39,12 +62,17 @@ def get_profile(file,pxsize,PA_aperture,PA_disk,inc,d,size,Nbins,**kwargs):
     # inc: disk's inclination (deg)
     # d: distance to the source (pc)
     # size: semi-major axis of the disk (AU)
+    # dr: width of the annulus (AU)
     # 
+    # The ouput is a matrix whose the rows and columns represent
+    # the position angle and the radial distance of the flux 
+    # measurement.
+    #
     ############################################################
 
 
     ############################################################
-    # Load data
+    # Load ALMA data
     hdulist=fits.open(file)
     data_obs=hdulist[0].data[0][0]
     xc=hdulist[0].header['CRPIX1']
@@ -53,7 +81,7 @@ def get_profile(file,pxsize,PA_aperture,PA_disk,inc,d,size,Nbins,**kwargs):
 
     ############################################################
     # Derived properties
-    angle_annulus=((PA_aperture-90.0)*units.deg).to(units.rad).value 
+    angle_annulus=((PA_disk-90.0)*units.deg).to(units.rad).value 
     e=np.sin((inc*units.deg).to(units.rad).value) 
     d_au=(d*units.pc).to(units.au).value # Distance (au)
     w=1.0
@@ -68,22 +96,24 @@ def get_profile(file,pxsize,PA_aperture,PA_disk,inc,d,size,Nbins,**kwargs):
     angular_lim=linear_lim/d_au # rad
     angular_lim=(angular_lim*units.rad).to(units.arcsec).value # arcsec
     pixel_lim=int(round(angular_lim/pxsize))
-    dr=topx(30.0,pxsize,d) 
+    dr=topx(dr,pxsize,d) 
     a_in_array=[]
-
     for i in np.arange(yc+dr,yc+0.5*pixel_lim,dr):
         a_in_array.append(i-xc)
     a_out_array=[i+dr for i in a_in_array]
     b_out_array=[i*(1-e**2)**0.5 for i in a_out_array]
-
+    a_in_array=np.array(a_in_array)
+    a_out_array=np.array(a_out_array)
     apertures=[EllipticalAnnulus((yc,xc),a_in=ain,a_out=aout,b_out=bout,theta=angle_annulus)
                for (ain,aout,bout) in zip(a_in_array,a_out_array,b_out_array)]
-        
+
+    print("Number of annular apertures: %d"%len(apertures))
+    
     # Do a check?
     plt.imshow(data_obs)
-    apertures[-1].plot(color='red',lw=1)
+    apertures[0].plot(color='red',lw=1)
     plt.show()
-        
+
     ############################################################
     # Define class "Bin"
     class Bin:
@@ -103,19 +133,30 @@ def get_profile(file,pxsize,PA_aperture,PA_disk,inc,d,size,Nbins,**kwargs):
             value=(self.theta_max-self.theta_min)*0.5+self.theta_min
             return value
 
-    ############################################################
-    # Creating array of bins
-    bin_list=[]
     thetas=np.linspace(0,2*np.pi,Nbins+1)
-    for i in range(0,Nbins):
-        sbin=Bin(i+1,thetas[i],thetas[i+1],[])
-        bin_list.append(sbin)
-
-    
     M=np.zeros((Nbins,len(apertures)))
 
-        
+    a_in_array=[i*pxsize*d for i in a_in_array]
+    a_out_array=[i*pxsize*d for i in a_out_array]
+    a_mid=np.array([(j-i)*0.5+i for (j,i) in zip(a_out_array,a_in_array)])
+    
+
     for ii in range(0,len(apertures)):
+
+        ############################################################
+        # Creating array of bins
+        bin_list=[]
+
+        for i in range(0,Nbins):
+            sbin=Bin(i+1,thetas[i],thetas[i+1],[])
+            bin_list.append(sbin)
+        
+        if ii==0:
+            midtheta=[]
+            midr=[]
+            for value in bin_list:
+                midtheta.append(value.getTheta())
+
         ############################################################
         # Creating aperture mask
         mask=apertures[ii].to_mask(method="center")
@@ -129,12 +170,13 @@ def get_profile(file,pxsize,PA_aperture,PA_disk,inc,d,size,Nbins,**kwargs):
         ############################################################
         # Extracting pixels located inside the aperture
         aperture_data=mask.multiply(data_obs)
-        
+        """
         # Do a check?
         plt.imshow(aperture_data)
         plt.colorbar()
         plt.show()
-           
+        """
+
         ############################################################
         # Creating array of pixel's index within the aperture 
         # relative to the star
@@ -160,18 +202,32 @@ def get_profile(file,pxsize,PA_aperture,PA_disk,inc,d,size,Nbins,**kwargs):
 
 
         ############################################################
-        # Writing azimuthal profile 
-        x=[]
-        y=[]
+        # Writing result
         j=0
         for value in bin_list:
             M[j][ii]=value.getFlux()
             j+=1
+            
+    for i in range(0,M.shape[0]):
+        M[i]=M[i]/max(M[i])
 
-    print(M)
 
+    ############################################################
+    # Plotting
+    fig=plt.figure(figsize=(5,12))
+    gs=gridspec.GridSpec(int(Nbins*0.5),1,hspace=0)
+    for i in range(0,int(Nbins*0.5)):
+        ax=plt.subplot(gs[i,0])
+        ax.plot(a_mid,np.reshape(M[i:i+1,:],M.shape[1]),'.',color="red")
+        ax.plot(-a_mid,np.reshape(M[i+int(0.5*Nbins):i+1+int(0.5*Nbins),:],M.shape[1]),'*',color="red")
+        ax.axvline(+74,0,1)
+        ax.axvline(-74,0,1)
+        ax.tick_params(labelleft=False,left=False)
+        ax.set_ylabel(r"%.1f"%((midtheta[i]*units.rad).to(units.deg).value))
+        ax.set_xlabel(r"$r$(AU)")
+    plt.show()
 
 
 get_profile("../PDS70/observations/PDS70_cont-final.fits",
-            0.020,158.6,158.6,49.7,113.43,120.0,4,average=True)
+            0.020,158.6,49.7,113.43,120.0,18,4,average=True)
 
