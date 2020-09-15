7ce49e81f817597568d3777a336b4771f23f0dd1
bportilla <b.a.portilla.revelo@rug.nl>
2020-09-15 11:49:08 +0200

    gomcmax

--- gomcmax.py
+++ gomcmax.py
@@ -6,28 +6,7 @@ import matplotlib.gridspec as gridspec
 from astropy.io import fits
 from astropy.coordinates import Angle
 import astropy.units as u
-"""
-cube=imagecube('/Users/users/bportilla/Documents/first_project/scripts/PDS70/observations/PDS70_cont-final.fits',FOV=3)
-#cube2=imagecube('/Users/users/bportilla/Documents/first_project/scripts/PDS70/observations/PDS70_cont-final.fits',FOV=3)
-#cube2=imagecube('/Users/users/bportilla/Documents/first_project/scripts/PDS70/observations/PDS70.fits',FOV=3)
-cube2=imagecube('/data/users/bportilla/runs/final_runs/1BS/run04_copy/alma_model_rotated.fits',FOV=3)
-xm, ym, dym = cube.radial_profile(inc=49.7,PA=158.6,dist=113.43,x0=0.02,y0=0.02)
-xm2, ym2, dym2 = cube2.radial_profile(inc=49.7,PA=158.6,dist=113.43)
-#x=[i*]
-#print(xm2)
-#sys.exit()
-plt.errorbar(xm,ym*1000,dym*1000,fmt='.',label="obs")
-plt.errorbar(xm2,ym2,dym2,fmt='.',label="mod")
-plt.legend()
-plt.show()
-sys.exit()
-f=open("gofishdat.dat","w")
-for (i,j,k) in zip(xm,ym,dym):
-    f.write("%.13e %.13e %.13e \n"%(i*113.43,j/np.nanmax(ym),k/np.nanmax(ym)))
-f.close()
-plt.errorbar(xm*113.43,ym/np.nanmax(ym),dym/np.nanmax(ym))
-plt.show()
-"""
+
 
 def get_profile(file,PA_disk,inc,d,padir,widir,dr):
     cube=imagecube(file,FOV=3)
@@ -45,7 +24,6 @@ drsample=0.02
 padisk=158.6
 incdisk=49.7
 padir=118.4
-#padir=-61.6
 widir=20.0
 nbins=1 # odd integer
 
