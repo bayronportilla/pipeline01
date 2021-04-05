1298427b0fc55c139b2aa701e68b1958aa955f1b
bportilla <b.a.portilla.revelo@rug.nl>
2021-04-05 13:01:02 +0200

    tmap

--- tmap.py
+++ tmap.py
@@ -9,5 +9,8 @@ import astropy.units as u
 import sys
 
 zones=mread.read_zones("../output/")
-fig=mplot.plot_midplane_zones_new(zones, "temp",vlim=[1,1600],plotGrid=True)
+fig=mplot.plot_midplane_zones_new(zones, "temp",vlim=[1,1600],plotGrid=False)
+plt.xlim(25,41)
+plt.ylim(9,27)
+#fig=mplot.plot_cuts_zones(zones,"temp",vlim=[1,1600],plotGrid=False)
 plt.show()
